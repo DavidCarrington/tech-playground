ad5bb72e91bc0ac912fa80ca4da85c980ad193ad
David Carrington <david.carrington@dixonscarphone.com>
2021-06-02 23:14:37 +0100

    Use detailed_errcodes from PHP Couchbase

--- application/index.php
+++ application/index.php
@@ -10,14 +10,27 @@ $authenticator = new PasswordAuthenticator();
 $authenticator->username($_ENV['COUCHBASE_ADMIN_USERNAME'])->password($_ENV['COUCHBASE_ADMIN_PASSWORD']);
 
 // Connect to Couchbase Server - using address of a KV (data) node
-$cluster = new CouchbaseCluster("couchbase://couchbase.one,couchbase.two,couchbase.three");
+$cluster = new CouchbaseCluster("couchbase://couchbase.one,couchbase.two,couchbase.three?detailed_errcodes=true");
 
 // Authenticate, then open bucket
 $cluster->authenticate($authenticator);
 $bucket = $cluster->openBucket($bucketName);
 
-// Store a document
 $key = substr(md5(microtime(true)), 0, 2);
+
+echo "Get\n";
+try {
+    $result = $bucket->get($key);
+} catch (\Couchbase\Exception $ex) {
+    if ($ex->getCode() == COUCHBASE_KEY_ENOENT) {
+        echo "Not found (BAU)\n";
+    } else {
+        throw $ex;
+    }
+}
+print_r($result);
+
+// Store a document
 echo "Storing u:king_arthur\n";
 $result = $bucket->upsert($key, array(
     "email" => "kingarthur@couchbase.com",
--- application/setup-couchbase.sh
+++ application/setup-couchbase.sh
@@ -1,6 +1,5 @@
 # Set up server 1
 curl -v http://couchbase.one:8091/pools/default -d memoryQuota=256 -d indexMemoryQuota=256
-#curl -v http://couchbase.one:8091/node/controller/setupServices -d services=kv%2cn1ql%2Cindex
 curl -v http://couchbase.one:8091/node/controller/setupServices -d services=kv
 curl -v http://couchbase.one:8091/settings/web -d port=8091 -d username=$COUCHBASE_ADMIN_USERNAME -d password=$COUCHBASE_ADMIN_PASSWORD
 curl -v -u $COUCHBASE_ADMIN_USERNAME:$COUCHBASE_ADMIN_PASSWORD http://couchbase.one:8091/pools/default/buckets -d name=my_bucket -d bucketType=couchbase -d ramQuotaMB=128 -d authType=sasl -d saslPassword=my_bucket_PASSWORD
